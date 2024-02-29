ca8f3354b37f3a3e6626cc1698e1f71a4c95d87e
Soniamy <marianna.marzullo@gmail.com>
2024-02-29 21:54:45 +0100

    creazione del form in create.blade.php e collegamento tra index e create attraverso il comics controller

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,9 +3,11 @@
 @section('page-title', 'Comics')
 
 @section('main-content')
- <h1>Elenco dei Fumetti</h1>
-
-    <div class="row">
+ <h1 class="text-center">Elenco dei Fumetti</h1>
+     <div class="mb-4">
+       <a href="{{route('comics.create')}}" class="btn btn-primary w-100 fs-5"> + Aggiungi</a>
+     </div>
+    <div class="row m-4">
         @foreach($comics as $comic)
             <div class="col-md-4 mb-4">
                 <div class="card">
@@ -16,7 +18,7 @@
                         <p class="card-text"><strong>Prezzo:</strong> {{ $comic->price}}</p>
                         <p class="card-text"><strong>Serie:</strong> {{ $comic->series}}</p>
                         <p class="card-text"><strong>Data di Vendita:</strong> {{ $comic->sale_date}}</p>
-                        <p class="card-text"><strong>Type:</strong> {{ $comic->type}}</p>
+                        <p class="card-text"><strong>Tipo:</strong> {{ $comic->type}}</p>
                         <p class="card-text"><strong>Artista/i:</strong> {{ $comic->artists}}</p>
                         <p class="card-text"><strong>Scrittore/i:</strong> {{ $comic->writers}}</p>
                          <a href="{{route('comics.show',['comic' => $comic->id])}}" class="btn btn-primary">Go to Comic</a>
